c41e2edefeb9d2ad37b306c845dbb7649bfd90a6
Deym0sss <danilmironenko41@gmail.com>
2024-04-16 17:41:13 +0300

    done

--- src/index.js
+++ src/index.js
@@ -22,9 +22,5 @@ let rerenderEntireTree = () => {
 };
 
 rerenderEntireTree(store.getState());
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
 
 reportWebVitals();
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,39 +1,46 @@
-const ADD_POST = 'ADD-POST'
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-let initialState =
-{ posts: [
-  { message: 'Hello there', amountOfLikes: 'Likes 12' },
-  { message: 'Escalator', amountOfLikes: 'Likes 28' },
-  { message: 'Mayonez', amountOfLikes: 'Likes 20' },
-  { message: 'First post', amountOfLikes: 'Likes 100' }
+let initialState = {
+  posts: [
+    { message: "Hello there", amountOfLikes: "Likes 12" },
+    { message: "Escalator", amountOfLikes: "Likes 28" },
+    { message: "Mayonez", amountOfLikes: "Likes 20" },
+    { message: "First post", amountOfLikes: "Likes 100" },
   ],
-  newPostText: 'Example'
-}
+  newPostText: "Example",
+};
 
-const profileReduser= (state = initialState, action) =>
-{
-  switch(action.type){
-    case ADD_POST:
-
+const profileReduser = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST: {
       let newPost = {
-                id: 5,
-                message: state.newPostText,
-                amountOfLikes: 'Likes ' + Math.round(Math.random() * 101)
-            }
-            state.posts.push(newPost)
-            state.newPostText=''
-            return(state)
+        id: 5,
+        message: state.newPostText,
+        amountOfLikes: "Likes " + Math.round(Math.random() * 101),
+      };
+      let stateCopy = { ...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.posts.push(newPost);
+      stateCopy.newPostText = "";
+      return stateCopy;
+    }
 
-    case UPDATE_NEW_POST_TEXT:
+    case UPDATE_NEW_POST_TEXT: {
+      let stateCopy = { ...state };
+      stateCopy.newPostText = action.newText;
+      return stateCopy;
+    }
 
-      state.newPostText = action.newText
-      return(state)
-
-    default: return(state)
+    default:
+      return state;
   }
-}
+};
 
-export const addPostActionCreator = () => {return {type: ADD_POST}}
-export const updateNewPostTextActionCreator =(text)=>{return{type: UPDATE_NEW_POST_TEXT, newText:text}}
-export default profileReduser
+export const addPostActionCreator = () => {
+  return { type: ADD_POST };
+};
+export const updateNewPostTextActionCreator = (text) => {
+  return { type: UPDATE_NEW_POST_TEXT, newText: text };
+};
+export default profileReduser;
