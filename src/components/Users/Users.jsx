573db27eebfde055f0b6f0f8cb41e1912631f19b
Deym0sss <danilmironenko41@gmail.com>
2024-04-26 17:13:02 +0300

    done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,12 +4,11 @@ import axios from "axios";
 import userDefault from "../../assets/images/image.png";
 
 export class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
-        props.setUsers(response.data.items);
+        this.props.setUsers(response.data.items);
       });
   }
   render() {
