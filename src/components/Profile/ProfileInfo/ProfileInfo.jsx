5ddd83e7d44ebb319571f1d244e7884abbbf1454
Deym0sss <danilmironenko41@gmail.com>
2024-04-29 19:29:48 +0300

    done x2

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,18 +1,73 @@
 import { Preloader } from "../../common/Preloader/Preloader";
 import styles from "./ProfileInfo.module.css";
+import { BsFillHandThumbsDownFill } from "react-icons/bs";
+import { BsFillHandThumbsUpFill } from "react-icons/bs";
+
 const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
   }
+  const entries = Object.entries(props.profile.contacts);
+  console.log(props.profile);
   return (
     <div>
       <div>
-        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+        <img
+          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+          alt="Nothing"
+        />
       </div>
+      <div style={{ display: "flex" }}>
+        <img src={props.profile.photos.large} alt="Nothing" />
+        <div style={{ marginLeft: "30px" }}>
+          <div
+            style={{
+              fontSize: "30px",
+              color: "yellow",
+              textShadow:
+                "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+            }}
+          >
+            {props.profile.fullName}
+          </div>
 
-      <div className={styles.description}>
-        <img src={props.profile.photos.large}></img>
-        ava + discription
+          <div style={{ color: "yellow" }}>{props.profile.aboutMe}</div>
+          {entries.map(([key, value]) => (
+            <div style={{ color: "yellow" }} key={key}>
+              <div>{key}:</div> <div>{value ? value : "-"}</div>
+            </div>
+          ))}
+        </div>
+
+        <div
+          style={{
+            width: "300px",
+            display: "flex",
+            flexDirection: "column",
+            color: "yellow",
+            marginLeft: "20px",
+            padding: "10px",
+          }}
+        >
+          {props.profile.lookingForAJob ? (
+            <>
+              <div style={{}}>
+                <span> Searching for job</span>
+
+                <BsFillHandThumbsUpFill />
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <span> Doesn`t searching for job</span>
+
+                <BsFillHandThumbsDownFill />
+              </div>
+            </>
+          )}
+          <span>{props.profile.lookingForAJobDescription}</span>
+        </div>
       </div>
     </div>
   );
